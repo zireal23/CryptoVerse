2a0aa6a265504df68d4401f0c152d1b9a0d21016
lightning-zeus <skasif232000@gmail.com>
2022-03-07 01:32:40 +0530

    Final Commit of the completed CryptoVerse app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, {useState, useEffect} from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MeneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined, MoneyCollectOutlined } from '@ant-design/icons';
+import icon from '../images/cryptocurrency.png';
+import "../App.css";
+
+
+const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleReSize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleReSize);
+    handleReSize();
+
+    return () => window.removeEventListener('resize', handleReSize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false);
+    } else
+      setActiveMenu(true);
+  }, [screenSize])
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Cryptoverse</Link>
+        </Typography.Title>
+        <Button className='menu-control-container' onClick={()=>setActiveMenu(!activeMenu)}>
+            <MenuOutlined/>
+        </Button>
+      </div>
+      {activeMenu && (
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">CryptoCurrencies</Link>
+          </Menu.Item>
+          {/* <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item> */}
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
+    </div>
+  );
+}
+
+export default Navbar
---
+++ src/services/cryptoapi.js
@@ -0,0 +1,53 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+const cryptoapiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+   "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_COIN_RANKING_API_KEY,
+};
+
+const createRequest = (url) => ({ url, headers: cryptoapiHeaders });
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getExchanges: builder.query({
+      query: () => createRequest(`/exchanges`),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timePeriod }) =>
+        createRequest(`/coin/${coinId}/history?timeperiod=${timePeriod}`),
+    }),
+  }),
+});  
+
+export const {
+    useGetCryptosQuery,
+    useGetCryptoDetailsQuery,
+    useGetCryptoHistoryQuery,
+    useGetExchangesQuery,
+} = cryptoApi;
+
+// var options = {
+//   method: "GET",
+//   url: "https://coinranking1.p.rapidapi.com/coins",
+//   params: {
+//     referenceCurrencyUuid: "yhjMzLPhuIDl",
+//     timePeriod: "24h",
+//     tiers: "1",
+//     orderBy: "marketCap",
+//     orderDirection: "desc",
+//     limit: "50",
+//     offset: "0",
+//   },
+//   headers: {
+//     "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+//     "x-rapidapi-key": "81b82b588fmshf1cd64975bd20acp10f561jsn0606cfe22d88",
+//   },
+// };
